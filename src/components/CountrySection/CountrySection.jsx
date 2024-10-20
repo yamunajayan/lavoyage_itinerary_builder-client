beccd693aea3018768879bb6a252e9316c17eccb
yamuna <yamunajayan@gmail.com>
2024-10-20 00:06:59 -0400

    add card component and card section

---
+++ src/components/CountrySection/CountrySection.jsx
@@ -0,0 +1,48 @@
+import "./CountrySection.scss";
+import CountryCard from "../countryCard/countryCard";
+import italyImage from "../../assets/photos/rome-2.jpeg";
+import egyptImage from "../../assets/photos/Egypt-1.jpeg";
+import franceImage from "../../assets/photos/France-2.jpeg";
+import thailandImage from "../../assets/photos/Thailand-1.avif";
+import indiaImage from "../../assets/photos/India-1.avif";
+import baliImage from "../../assets/photos/Bali-1.webp";
+import chevronRight from "../../assets/logos/chevron-right.svg";
+import chevronLeft from "../../assets/logos/chevron-left.svg";
+
+const CountrySection = () => {
+  const countryNames = [
+    "ITALY",
+    "THAILAND",
+    "INDIA",
+    "EGYPT",
+    "BALI",
+    "FRANCE",
+  ];
+  const countryImages = [
+    italyImage,
+    thailandImage,
+    indiaImage,
+    egyptImage,
+    baliImage,
+    franceImage,
+  ];
+  return (
+    <section className="country-section">
+      <h2 className="country-title">Most Popular Travel Destinations</h2>
+      <ul className="country-section__list">
+        {countryNames.map((country, index) => {
+          return (
+            <li key={country} className="country-section__item">
+              <CountryCard
+                countryName={country}
+                countryImage={countryImages[index]}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default CountrySection;
